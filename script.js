f749a9b60eab6391460302373aa0ed2a435af827
Austin Donovan <32428946+Iskarr@users.noreply.github.com>
2026-04-28 13:06:12 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// BUG: Attempting to add a listener to a variable that hasn't been defined yet
+button.addEventListener("click", () => {
+    alert("Order Placed!");
+});
+
+// BUG: The developer forgot to actually 'select' the element first
+const button = document.getElementById("order-btn");
